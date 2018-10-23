06239dcc122c969bb4ff0a645e1eb1d741fa807f
Hurricane36 <43070976+Hurricane36@users.noreply.github.com>
2018-10-23 09:22:29 -0600

    added format algortithm

--- Controller/AlgorithhmViewController.swift
+++ Controller/AlgorithhmViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 public class AlgorithmViewController: UIViewController {
 
     @IBOutlet weak var SwiftImage: UIImageView!
-    @IBOutlet weak var AlgorithimText: UILabel!
+    @IBOutlet weak var algorithmText: UILabel!
    public override func viewDidLoad() -> Void
     {
         super.viewDidLoad()
@@ -20,7 +20,40 @@ public class AlgorithmViewController: UIViewController {
     }
     private func formatAlgorithm() -> Void
     {
+        let stepOne : String = "Click on create new Xcode project and select Single view app."
+        let stepTwo : String = "Create a new folder, name it View, and put the Main.Storyboard andLaunchScreen.storyboard files into the folder."
+        let stepThree : String =  "Create another file, name is resources, and put the Assets folder,AppDelegate, and info.plist files into the folder."
+        let stepFour : String = "Create a new folder, name it controller, and put the viewController file into it"
+        let stepFive : String = "Double click on the project file in xcode and click on choose “info.plist” and select your “info.plist” file."
+        let algorithm = [stepOne, stepTwo, stepThree, stepFour, stepFive]
         
+        let attributesDictionary = [NSAttributedStringKey.font : algorithmText.font]
+        let fullAttributedString = NSMutableAttributedString(string: title, attributes: attributesDictionary)
+        
+        for step in algorithm
+        {
+            let bullet : String = "💣"
+            let formattedStep : String = "n\(bullet) \(step)"
+            let attributedStringStep : NSAttributedString = NSMutableAttributedString(string: formattedStep)
+            let outlineStyle = createOutlineStyle()
+            
+            attributedStringStep.attributes([NSAttributedStringKey.paragraphStyle : outlineStyle], range: NSMakeRange(0,attributedStringStep.length))
+            
+            fullAttributedString.append(attributedStringStep)
+        }
+        algorithmText.attributedText = fullAttributedString
+    }
+    
+
+    private func createOutlineStyle() -> NSParagraphStyle
+    {
+        let outlineStyle : NSMutableParagraphStyle = NSMutableParagraphStyle()
+        outlineStyle.alignment = .left
+        outlineStyle.defaultTabInterval = 15
+        outlineStyle.firstLineHeadIndent = 20
+        outlineStyle.headIndent = 35
+        
+        return outlineStyle
     }
 }
 
